b864ff22b585f8b3c03955e7c179f77c1caa9fe0
jorge <jorgito2577@gmail.com>
2025-05-18 19:36:59 -0400

    Agregue el segundo Programa solo intruciones

---
+++ manipulaciondListas.py
@@ -0,0 +1,23 @@
+'''
+Programa 2: Manipulación de Listas
+
+Crea un programa en Python que realice varias operaciones en una lista de números.
+
+El programa debe:
+
+    Pedir al usuario que ingrese una serie de números, separados por espacios.
+    Convertir la entrada en una lista de enteros.
+    Realizar las siguientes operaciones y mostrar los resultados:
+        Calcular la suma de todos los números. 
+        Encontrar los valores máximo y mínimo.
+        Calcular el promedio (media) de los números.
+        Eliminar cualquier número duplicado y mostrar la lista actualizada.
+        Ordenar la lista en orden ascendente.
+        Crear una nueva lista que contenga solo los números pares de la lista original.
+
+Requisitos:
+    Utiliza métodos de lista y funciones incorporadas para realizar las operaciones.
+    Implementa comprensión de listas para crear la lista de números pares.
+    Usa un conjunto (set) para eliminar duplicados.
+    Formatea la salida de manera clara, mostrando el resultado de cada operación.
+    '''
